334af889d2f7cc5e0665f25acd7e98ad075536ba
Daniel Buckley <rc0038739@student.riversidecollege.ac.uk>
2020-02-11 11:31:42 +0000

    Add project files.

---
+++ Numbers/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + ""
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
